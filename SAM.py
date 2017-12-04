98c93a63f4e193e1a30e5e7c38489da116b02420
Ferox <ferox.dragon@gmail.com>
2017-12-04 08:54:37 +0800

    commet for debuging

--- SAM.py
+++ SAM.py
@@ -6,17 +6,17 @@ class App:
 		self.sensor = GTSensor('/dev/ttyAMA0', timeout=2, baudrate=9600)
 
 		_initialization_response = self.sensor.initialize(True)
-		time.sleep(0.5)
-		self.sensor.LED(True)
-		println(_initialization_response)
-		self.sensor.LED(False)
+		# time.sleep(0.5)
+		# self.sensor.LED(True)
+		# println(_initialization_response)
+		# self.sensor.LED(False)
 
-		time.sleep(0.5)
-		self.sensor.LED(True)
-		print ("Setting baudrate from 9600 to 57600")
-		baudrateResult = self.sensor.setBaudrate(56700)
-		print(baudrateResult)
-		self.sensor.LED(False)
+		# time.sleep(0.5)
+		# self.sensor.LED(True)
+		# print ("Setting baudrate from 9600 to 57600")
+		# baudrateResult = self.sensor.setBaudrate(56700)
+		# print(baudrateResult)
+		# self.sensor.LED(False)
 
 app=App()
 
