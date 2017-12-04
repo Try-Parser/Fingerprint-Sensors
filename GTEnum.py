ab57472f303a46d65e2b765d0c6252f85638e24d
Ferox <ferox.dragon@gmail.com>
2017-12-04 16:11:33 +0800

    test

--- GTEnum.py
+++ GTEnum.py
@@ -62,24 +62,24 @@ class GT521F5(Enum):
 
     ERRORS = {
         # Error Response 
-        0x1001: 'NACK_TIMEOUT'               # (Obsolete) Capture timeout
-        0x1002: 'NACK_INVALID_BAUDRATE'      # (Obsolete) Invalid serial baud rate
-        0x1003: 'NACK_INVALID_POS'           # The specified ID is not in range[0,199]
-        0x1004: 'NACK_IS_NOT_USED'           # The specified ID is not used
-        0x1005: 'NACK_IS_ALREADY_USED'       # The specified ID is already in use
-        0x1006: 'NACK_COMM_ERR'              # Communication error
-        0x1007: 'NACK_VERIFY_FAILED'         # 1:1 Verification Failure
-        0x1008: 'NACK_IDENTIFY_FAILED'       # 1:N Identification Failure
-        0x1009: 'NACK_DB_IS_FULL'            # The database is full
-        0x100A: 'NACK_DB_IS_EMPTY'           # The database is empty
-        0x100B: 'NACK_TURN_ERR'              # (Obsolete) Invalid order of the enrollment
+        0x1001: 'NACK_TIMEOUT',               # (Obsolete) Capture timeout
+        0x1002: 'NACK_INVALID_BAUDRATE',      # (Obsolete) Invalid serial baud rate
+        0x1003: 'NACK_INVALID_POS',           # The specified ID is not in range[0,199]
+        0x1004: 'NACK_IS_NOT_USED',           # The specified ID is not used
+        0x1005: 'NACK_IS_ALREADY_USED',       # The specified ID is already in use
+        0x1006: 'NACK_COMM_ERR',              # Communication error
+        0x1007: 'NACK_VERIFY_FAILED',         # 1:1 Verification Failure
+        0x1008: 'NACK_IDENTIFY_FAILED',       # 1:N Identification Failure
+        0x1009: 'NACK_DB_IS_FULL',            # The database is full
+        0x100A: 'NACK_DB_IS_EMPTY',           # The database is empty
+        0x100B: 'NACK_TURN_ERR',              # (Obsolete) Invalid order of the enrollment
                                              # (EnrollStart->Enroll1->Enroll2->Enroll3)
-        0x100C: 'NACK_BAD_FINGER'            # Fingerprint is too bad
-        0x100D: 'NACK_ENROLL_FAILED'         # Enrollment Failure
-        0x100E: 'NACK_IS_NOT_SUPPORTED'      # The command is not supported
-        0x100F: 'NACK_DEV_ERR'               # Device error: probably Crypto-Chip is faulty (Wrong checksum ~Z)
-        0x1010: 'NACK_CAPTURE_CANCELED'      # (Obsolete) Capturing was canceled
-        0x1011: 'NACK_INVALID_PARAM'         # nvalid parameter
+        0x100C: 'NACK_BAD_FINGER',            # Fingerprint is too bad
+        0x100D: 'NACK_ENROLL_FAILED',         # Enrollment Failure
+        0x100E: 'NACK_IS_NOT_SUPPORTED',      # The command is not supported
+        0x100F: 'NACK_DEV_ERR',               # Device error: probably Crypto-Chip is faulty (Wrong checksum ~Z)
+        0x1010: 'NACK_CAPTURE_CANCELED',      # (Obsolete) Capturing was canceled
+        0x1011: 'NACK_INVALID_PARAM',         # nvalid parameter
         0x1012: 'NACK_FINGER_IS_NOT_PRESSED' # Finger is not pressed
     }
 
