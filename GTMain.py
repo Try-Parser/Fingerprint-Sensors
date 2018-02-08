59c51da97a98183f59d699b2f56d62e9e970766e
Ferox <ferox.dragon@gmail.com>
2018-02-08 15:03:43 +0800

    add all edited files

--- GTMain.py
+++ GTMain.py
@@ -23,12 +23,13 @@ class App:
 		self.sensor.LED(False)	
 
 		self.template = ""
+		self.ctl = False
 
 	def __capture_the_lights__(self): 
 		while True:
 			procced = False
 
-			if self.stopScan:
+			if self.ctl:
 				return False
 
 			if self.sensor.senseFinger()[0]['Parameter'] == 0:
