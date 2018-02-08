7e22eb2b6287e440373c5780db084a9f1755e3a8
Ferox <ferox.dragon@gmail.com>
2018-02-08 11:03:02 +0800

    add all edited files

--- rascan.py
+++ rascan.py
@@ -47,28 +47,30 @@ class Rascan:
 
 	def on_message(self, ws, message):
 		templates = json.loads(message)
-		print(templates)
 		resp = templates["response"]
-		if templates["message"] == "NFP":
-			self.templates.append(resp)
-			print("Enrollment Starting")
-			t4 = threading.Thread(target=self.app.scanLoop, args=(self,))
-			t4.start()
-		else:
-			if templates["success"] == True and len(resp["results"]) > 0:
-				print("Inserting template to memory")
-				self.templates.append(resp["results"][0])
-				if resp["from"] == resp["total"]-1:
-					print("Enrollment Starting")
-					t3 = threading.Thread(target=self.app.scanLoop, args=(self,))
-					t3.start()
-				else:
-					print(resp["from"])
-					print(resp["total"]-1)
-			else:
+		if resp == "ISR": 
+			if templates["message"] == "NFP":
+				self.templates.append(resp)
 				print("Enrollment Starting")
-				t2 = threading.Thread(target=self.app.scanLoop, args=(self,))
-				t2.start()
+				t4 = threading.Thread(target=self.app.scanLoop, args=(self,))
+				t4.start()
+			else:
+				if templates["success"] == True and len(resp["results"]) > 0:
+					print("Inserting template to memory")
+					self.templates.append(resp["results"][0])
+					if resp["from"] == resp["total"]-1:
+						print("Enrollment Starting")
+						t3 = threading.Thread(target=self.app.scanLoop, args=(self,))
+						t3.start()
+					else:
+						print(resp["from"])
+						print(resp["total"]-1)
+				else:
+					print("Enrollment Starting")
+					t2 = threading.Thread(target=self.app.scanLoop, args=(self,))
+					t2.start()
+		else:
+			print("aw")
 
 	def on_error(self, ws, error):
 		print(error)
