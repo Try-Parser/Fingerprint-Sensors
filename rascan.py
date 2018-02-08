f9f66c833ad55f1d47a35be5f4f7c5a355b92099
Ferox <ferox.dragon@gmail.com>
2018-02-08 16:13:12 +0800

    add all edited files

--- rascan.py
+++ rascan.py
@@ -51,7 +51,7 @@ class Rascan:
 		print(templates)
 		resp = templates["response"]
 		print(resp)
-		if resp != "ISR" and resp !="re-init": 
+		if resp != "ISR" and resp != "re-init": 
 			if templates["message"] == "NFP":
 				self.templates.append(resp)
 				print("Check Starting")
@@ -75,7 +75,7 @@ class Rascan:
 		elif resp == "re-init":
 			print("Re-initializing Sensor")
 			self.app.stopScan = True
-			time.sleep(1)
+			time.sleep(1.5)
 			self.app.stopScan = False
 			self.templates = []
 			self.initialize()
@@ -83,7 +83,7 @@ class Rascan:
 			self.app.sensor.LED(False)
 			print("Enrollment Starting")
 			self.app.stopScan = True
-			time.sleep(1)
+			time.sleep(1.5)
 			self.app.stopScan = False
 			e1 = threading.Thread(target=self.app.enroll, args=(self.ws,))
 			e1.start()
