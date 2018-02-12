3ead2cc33fc12137d162a2bd1c9fa3fb7d19f110
Ferox <ferox.dragon@gmail.com>
2018-02-12 19:27:51 +0800

    Add Func

--- rascan.py
+++ rascan.py
@@ -39,6 +39,7 @@ class Rascan:
 				self.sth[self.ctr].start()
 				self.sth[self.ctr].join()
 				self.ctr += 1
+				time.sleep(2)
 				self.app.stopScan = False
 				print("Check Starting")
 				threading.Thread(name=str(uuid.uuid4()), target=self.app.scan, args=()).start()
