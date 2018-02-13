017c09aa29f0ec2764a7edc4fe76a479ce61972c
Ferox <ferox.dragon@gmail.com>
2018-02-13 16:53:02 +0800

    13/02/2018

--- rascan.py
+++ rascan.py
@@ -68,9 +68,9 @@ class Rascan:
 					print("Check Starting")
 					threading.Thread(name=str(uuid.uuid4()), target=self.app.scan, args=()).start()
 
-	    if resp == "run_scan":
+		if resp == "run_scan":
 			print("Check Starting")
-	    	threading.Thread(name=str(uuid.uuid4()), target=self.app.scan, args=()).start()
+			threading.Thread(name=str(uuid.uuid4()), target=self.app.scan, args=()).start()
 
 		if resp == "RIS":
 			print("Re-initializing Sensor")
