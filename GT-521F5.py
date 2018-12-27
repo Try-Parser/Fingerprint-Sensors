ed8e075f31e058e6f66ac394f4d184004553cf91
Franklin Embate <ferox.dragon@gmail.com>
2018-12-27 18:38:21 +0800

    sd

--- GT-521F5.py
+++ GT-521F5.py
@@ -16,12 +16,12 @@ CMD_Code_2 = 0xAA
 
 ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=2)
 
-
 GPIO.setmode(GPIO.BCM)
 INPUT_PIN = 17
 INPUT_PIN2 = 18
+
 GPIO.setup(INPUT_PIN, GPIO.IN)
-GPIO.setup(INPUT_PIN2, GPIO.IN)
+GPIO.setup(INPUT_PIN2, GPIO.OUT)
 
 while True:
 	input1 = GPIO.input(INPUT_PIN)
