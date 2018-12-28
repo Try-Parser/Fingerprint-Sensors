f48e2f5b5776cbf1e2dea551ea6e7be0da29ef30
Franklin Embate <ferox.dragon@gmail.com>
2018-12-28 10:14:07 +0800

    a

--- GT-521F5.py
+++ GT-521F5.py
@@ -2,7 +2,7 @@ import serial
 import time
 import struct
 import RPi.GPIO as GPIO
-
+from time import sleep
 global ser
 
 comm_struct = lambda: '<BBHIH'
@@ -23,81 +23,82 @@ INPUT_PIN2 = 18
 GPIO.setup(INPUT_PIN, GPIO.OUT)
 GPIO.setup(INPUT_PIN2, GPIO.OUT)
 
+GPIO.add_event_detect(INPUT_PIN2, GPIO.FALLING, callback=inputLow, bouncetime=200);
+
+def inputLow(channel):
+	print(channel);
+    print('0');
+
 while True:
-	input1 = GPIO.input(INPUT_PIN)
-	print(str(input1) + " INPUT PIN")
-	input2 = GPIO.input(INPUT_PIN2)
-	print(str(input2) + " INPUT PIN2")
-
-	if input1 and input2:
-		break;
+	print('3.3');
+	sleep(1);  
 
 
-time.sleep(1)
-cmd = 0x22
-ID = 1
-deviceID = 0x01
+# time.sleep(1)
+# cmd = 0x22
+# ID = 1
+# deviceID = 0x01
 
-def writePacket(cmd, param, deviceID = deviceID):
-	packet = bytearray(struct.pack(comm_struct(), 0x55, 0xAA, deviceID, param, cmd))
-	checksum = sum(packet)
-	packet += bytearray(struct.pack(checksum_struct(), checksum))
+# def writePacket(cmd, param, deviceID = deviceID):
+# 	packet = bytearray(struct.pack(comm_struct(), 0x55, 0xAA, deviceID, param, cmd))
+# 	checksum = sum(packet)
+# 	packet += bytearray(struct.pack(checksum_struct(), checksum))
 
-	result = len(packet) == ser.write(packet)
-	ser.flush()
+# 	result = len(packet) == ser.write(packet)
+# 	ser.flush()
 
-	return result
+# 	return result
 
-def receivedPacket(packetLn = 12):
-	while not ser.readable():
-		print("aw")
+# def receivedPacket(packetLn = 12):
+# 	while not ser.readable():
+# 		print("aw")
 
-	rxPacket = ser.read(packetLn)
-	print(rxPacket)
+# 	rxPacket = ser.read(packetLn)
+# 	print(rxPacket)
 
-def onLED(tf):
-	result = writePacket(0x12, tf*1)
+# def onLED(tf):
+# 	result = writePacket(0x12, tf*1)
 
-	if result:
-		receivedPacket()
+# 	if result:
+# 		receivedPacket()
 
+
+# # ser.timeout = 10
+
+# result = writePacket(0x01, 0x0)
+# if result:
+# 	receivedPacket()
+# time.sleep(0.5)
+
+
+# result = writePacket(0x04, 57600)
+# if result:
+# 	ser.baudrate = 57600
+# 	receivedPacket()
+# time.sleep(0.5)
+
+
+# onLED(True)
+
+# _ = input("Press Enter to continue...")
+# ser.timeout = 10
+# result = writePacket(0x60, 0x01)
+
+# if result:
+# 	receivedPacket()
+# 	ser.timeout = 5
+
+# onLED(False)
+
+
+# print ("Generating Template..")
+
+# result = writePacket(0x61, 0x00)
 
 # ser.timeout = 10
 
-result = writePacket(0x01, 0x0)
-if result:
-	receivedPacket()
-time.sleep(0.5)
-
-
-result = writePacket(0x04, 57600)
-if result:
-	ser.baudrate = 57600
-	receivedPacket()
-time.sleep(0.5)
-
-
-onLED(True)
-
-_ = input("Press Enter to continue...")
-ser.timeout = 10
-result = writePacket(0x60, 0x01)
-
-if result:
-	receivedPacket()
-	ser.timeout = 5
-
-onLED(False)
-
-
-print ("Generating Template..")
-
-result = writePacket(0x61, 0x00)
-
-ser.timeout = 10
-
-if result:
-	rxPacket = ser.read(1+1+2+498+2)
-	ser.timeout = 5
-	print (rxPacket)
+# if result:
+# 	rxPacket = ser.read(1+1+2+498+2)
+# 	ser.timeout = 5
+# 	print (rxPacket)
 
